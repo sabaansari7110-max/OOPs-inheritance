2265b1cccd5fc6906617e2410ce349f29a3c927a
Saba Ansari <sabaansari7110@gmail.com>
2026-04-08 11:59:53 +0530

    Add files via upload

---
+++ practice set/04_complex_numbers_with_overload_operators.py
@@ -0,0 +1,27 @@
+# class 'complex, to represent complex numbers, along with overload operators '+' and '*'.whic adds and multiplies them.
+
+
+class complex:
+    def __init__(self, r, i):
+        self.r= r
+        self.i= i
+
+
+    def __add__(self, c2):
+        return complex(self.r + c2.r, self.i + c2.i)
+
+
+    def __mul__(self, c2):
+        real_part= self.r * c2.r - self.i * c2.i
+        img_part= self.r * c2.i + self.i * c2.r
+        return complex(real_part, img_part)
+
+    def __str__(self):
+        return f"{self.r} + {self.i}i"
+
+
+
+c1= complex(1,2)
+c2= complex(3,4)
+print(c1 + c2)
+print(c1 * c2)
